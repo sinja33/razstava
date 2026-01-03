b374ede92b9283c4fc4e8c00de38276512fb030f
Sinja Kocica <sinja.kocica@gmail.com>
2026-01-03 20:44:23 +0100

    Planes sensed

---
+++ Assets/Scripts/ScenePermissionRequester.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class ScenePermissionRequester : MonoBehaviour
+{
+    const string SCENE_PERMISSION = "com.oculus.permission.USE_SCENE";
+    
+    [SerializeField] private ARPlaneManager planeManager;
+
+    void Start()
+    {
+        // Disable plane manager until we have permission
+        if (planeManager != null)
+            planeManager.enabled = false;
+            
+        RequestPermission();
+    }
+
+    void RequestPermission()
+    {
+#if UNITY_ANDROID
+        if (Permission.HasUserAuthorizedPermission(SCENE_PERMISSION))
+        {
+            Debug.Log("Scene permission already granted");
+            EnableSpatialFeatures();
+        }
+        else
+        {
+            Debug.Log("Requesting scene permission...");
+            var callbacks = new PermissionCallbacks();
+            callbacks.PermissionGranted += OnPermissionGranted;
+            callbacks.PermissionDenied += OnPermissionDenied;
+            Permission.RequestUserPermission(SCENE_PERMISSION, callbacks);
+        }
+#endif
+    }
+
+    void OnPermissionGranted(string permission)
+    {
+        Debug.Log("Permission granted: " + permission);
+        EnableSpatialFeatures();
+    }
+
+    void OnPermissionDenied(string permission)
+    {
+        Debug.Log("Permission denied: " + permission);
+    }
+
+    void EnableSpatialFeatures()
+    {
+        if (planeManager != null)
+            planeManager.enabled = true;
+    }
+}
