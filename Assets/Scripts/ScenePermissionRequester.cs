eef3b6c77af97aba2c1ed31419ebb9ba1ef6d5cb
Sinja Kocica <sinja.kocica@gmail.com>
2026-02-18 02:00:58 +0100

    Tried building blocks but didnt work, now on unlit shaders for stereo problems

--- Assets/Scripts/ScenePermissionRequester.cs
+++ Assets/Scripts/ScenePermissionRequester.cs
@@ -1,5 +1,63 @@
+// using UnityEngine;
+// using UnityEngine.XR.ARFoundation;
+
+// #if UNITY_ANDROID
+// using UnityEngine.Android;
+// #endif
+
+// public class ScenePermissionRequester : MonoBehaviour
+// {
+//     const string SCENE_PERMISSION = "com.oculus.permission.USE_SCENE";
+    
+//     [SerializeField] private ARPlaneManager planeManager;
+
+//     void Start()
+//     {
+//         // Disable plane manager until we have permission
+//         if (planeManager != null)
+//             planeManager.enabled = false;
+            
+//         RequestPermission();
+//     }
+
+//     void RequestPermission()
+//     {
+// #if UNITY_ANDROID
+//         if (Permission.HasUserAuthorizedPermission(SCENE_PERMISSION))
+//         {
+//             Debug.Log("Scene permission already granted");
+//             EnableSpatialFeatures();
+//         }
+//         else
+//         {
+//             Debug.Log("Requesting scene permission...");
+//             var callbacks = new PermissionCallbacks();
+//             callbacks.PermissionGranted += OnPermissionGranted;
+//             callbacks.PermissionDenied += OnPermissionDenied;
+//             Permission.RequestUserPermission(SCENE_PERMISSION, callbacks);
+//         }
+// #endif
+//     }
+
+//     void OnPermissionGranted(string permission)
+//     {
+//         Debug.Log("Permission granted: " + permission);
+//         EnableSpatialFeatures();
+//     }
+
+//     void OnPermissionDenied(string permission)
+//     {
+//         Debug.Log("Permission denied: " + permission);
+//     }
+
+//     void EnableSpatialFeatures()
+//     {
+//         if (planeManager != null)
+//             planeManager.enabled = true;
+//     }
+// }
+
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
 
 #if UNITY_ANDROID
 using UnityEngine.Android;
@@ -8,15 +66,10 @@ using UnityEngine.Android;
 public class ScenePermissionRequester : MonoBehaviour
 {
     const string SCENE_PERMISSION = "com.oculus.permission.USE_SCENE";
-    
-    [SerializeField] private ARPlaneManager planeManager;
 
     void Start()
     {
-        // Disable plane manager until we have permission
-        if (planeManager != null)
-            planeManager.enabled = false;
-            
+        Debug.Log("ScenePermissionRequester: Start called");
         RequestPermission();
     }
 
@@ -26,33 +79,12 @@ public class ScenePermissionRequester : MonoBehaviour
         if (Permission.HasUserAuthorizedPermission(SCENE_PERMISSION))
         {
             Debug.Log("Scene permission already granted");
-            EnableSpatialFeatures();
         }
         else
         {
             Debug.Log("Requesting scene permission...");
-            var callbacks = new PermissionCallbacks();
-            callbacks.PermissionGranted += OnPermissionGranted;
-            callbacks.PermissionDenied += OnPermissionDenied;
-            Permission.RequestUserPermission(SCENE_PERMISSION, callbacks);
+            Permission.RequestUserPermission(SCENE_PERMISSION);
         }
 #endif
-    }
-
-    void OnPermissionGranted(string permission)
-    {
-        Debug.Log("Permission granted: " + permission);
-        EnableSpatialFeatures();
-    }
-
-    void OnPermissionDenied(string permission)
-    {
-        Debug.Log("Permission denied: " + permission);
-    }
-
-    void EnableSpatialFeatures()
-    {
-        if (planeManager != null)
-            planeManager.enabled = true;
     }
 }
